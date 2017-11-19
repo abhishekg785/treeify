5437546ba525770b7888f1ad83686fb10930b26c
ABHISHEK GOSWAMI ( Hiro ) <abhishek@ddproperty.com>
2017-11-20 05:29:21 +0700

    recursion to get files in dir working and tree formed

--- demo/mod1.js
+++ demo/mod1.js
@@ -3,9 +3,10 @@ const treeify = require('../build/index');
 // pass the dir name
 const obj = new treeify.Treeify('../build');
 
-obj.readDir()
+obj.treeify()
 .then((data)=>{
     console.log(data);
+    console.log(data.ram.shyam.bhanu.fname);
 })
 .catch((error)=>{
     console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,49 +1,72 @@
 /**
  * treeify: a simple manager to treeify the directory having modules
- * 
+ *
  * @author: abhishek goswami ( hiro )
  * abhishekg785@gmail.com
  */
 
 import path from 'path';
 import fs from 'fs';
-import merge from 'merge'; // using for merging the objects when needed
-import type from 'component-type'; // for checking types when needed
 
 class Treeify {
 
-    // get the name of the directory here
-    constructor(directory) {
+  // get the name of the directory here
+  constructor(directory) {
 
-        // maybe a relative path or an absolute path
-        this.inputDir = directory;
+    // maybe a relative path or an absolute path
+    this.inputDir = directory;
 
-        // the path of the module in which it has been required or imported
-        this.parentDir = path.dirname(module.parent.filename);
+    // the path of the module in which it has been required or imported
+    this.parentDir = path.dirname(module.parent.filename);
+
+    this.resolvedPath = path.resolve(this.parentDir, this.inputDir);
+  }
+
+  async treeify() {
+    let fileStatus = await this.checkDirExistStatus();
+    if (!fileStatus) {
+      throw new Error('No such directory exists!');
     }
+    let genTree = this.createTree(this.resolvedPath);
+    return genTree;
+  }
 
-    /**
-     * 
-     */
-    async readDir() {
-        return await this.checkDirExistStatus();
-    }
+  // this function create the required tree using
+  // a little magic of recursion :P
+  // pass the dir here and it will recursively get the files
+  // in the dir and require the modules to form the tree
+  createTree(dir) {
+    let genTree = {};
+    fs.readdirSync(dir).forEach(file => {
+      let filePath = path.join(dir, file);
+      if (fs.lstatSync(filePath).isDirectory()) {
+        return genTree[file] = this.createTree(filePath);
+      }
 
-    /**
-     * eheck for the given dir exists or not
-     */
-    checkDirExistStatus() {
-        return new Promise((res, rej) => {
-            let resolvedPath = path.resolve(this.parentDir, this.inputDir);
-            fs.stat(resolvedPath, (err, stats) => {
-                if(err && err.errno == '-2') { // error code for not exists
-                    rej(new Error('No such directory!'));
-                }
-                res(true);
-            });
-        });
-    }
-    
+      let requireFile = require(filePath);
+      Object.getOwnPropertyNames(requireFile).forEach((key) => {
+        genTree[key] = requireFile[key];
+      });
+    });
+
+    return genTree;
+  }
+
+  /**
+   * check for the given dir exists or not
+   */
+  checkDirExistStatus() {
+    return new Promise((res, rej) => {
+      let resolvedPath = path.resolve(this.parentDir, this.inputDir);
+      fs.stat(resolvedPath, (err, stats) => {
+        if(err && err.errno == '-2') { // error code for not exists
+          res(false);
+        }
+        res(true);
+      });
+    });
+  }
+
 }
 
 export { Treeify };
